2efcaf2a59aceda003a27724ef20702fcff2b5f7
elvinoza <elvinoza@gmail.com>
2016-11-28 21:35:34 +0200

    database migrations

---
+++ database/migrations/2016_11_28_191927_create_autos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number', 6)->unique();
+            $table->integer('car_id');
+            $table->integer('model_id');
+            $table->integer('color_id');
+            $table->dateTime('last_search_time');
+            $table->integer('views');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('autos');
+    }
+}
